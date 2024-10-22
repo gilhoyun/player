d8283226c98e60d0e7caa40da0098beea0ad5215
gilhoyun <gilhoyun04@gmail.com>
2024-10-22 11:40:26 +0900

    작업 2. doModify, doDelete

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -11,39 +11,102 @@ import com.example.demo.dto.Article;
 
 @Controller
 public class UsrArticleController {
-	
-	
+
 	private int lastArticleId;
-	
+
 	private List<Article> articles;
-	
+
 	public UsrArticleController() {
 		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();	
+		this.articles = new ArrayList<>();
 	}
-	
+
 	@GetMapping("/usr/article/doWrite")
 	@ResponseBody
-	public Article doWrite(String title,  String body) {
-		
+	public Article doWrite(String title, String body) {
+
 		lastArticleId++;
-	
+
 		Article article = new Article(lastArticleId, title, body);
-		
+
 		articles.add(article);
-		
+
 		return article;
 	}
-	
+
 	@GetMapping("/usr/article/showList")
 	@ResponseBody
-	public List<Article> showList() {	
+	public List<Article> showList() {
 		return articles;
-		
+
 	}
-	
-	
-	
+
+	@GetMapping("usr/article/showDetail")
+	@ResponseBody
+	public Object showDetail(int id) {
+
+		Article foundArticle = null;
+
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				foundArticle = article;
+				break;
+			}
+		}
+
+		if (foundArticle == null) {
+			return id + "번 게시물은 존개하지 않습니다.";
+		}
+
+		return foundArticle;
+
+	}
+
+	@GetMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+
+		Article foundArticle = null;
+
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				foundArticle = article;
+				break;
+			}
+		}
+				
+		if (foundArticle == null) {
+			return id + "번 게시물은 존개하지 않습니다.";
+		}	
+		
+		foundArticle.setTitle(title);
+		foundArticle.setBody(body);
+
+		return id + "번 게시물을 수정했습니다.";
+
+	}
+
+	@GetMapping("/usr/article/doDelete")
+	@ResponseBody
+	public Object doDelete(int id) {
+		
+		Article foundArticle = null;
+
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				foundArticle = article;
+				break;
+			}
+		}
+		
+		if (foundArticle == null) {
+			return id + "번 게시물은 존개하지 않습니다.";
+		}
+			
+		articles.remove(foundArticle);
+		
+		return id + "번 게시물을 삭제했습니다.";
+
+	}
+
 }
-
-
